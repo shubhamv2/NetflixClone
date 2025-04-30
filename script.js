e76cb62a4f0975dcc82927338680832d6c332a6a
Shubham <shubhamvairagade91@gmail.com>
2025-04-30 14:55:12 +0530

    lnaguage button hover effect and pop up effect functionality added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const inputLable = document.querySelector('.input-lable');
 const emailBox = document.getElementById('email-box');
+const emailBoxContainer = document.querySelector('.email-box-container');
 
 const myfunc = ()=>{
     inputLable.classList.add("event-class")
+    emailBoxContainer.classList.add('outline-add')
     
 }
 
@@ -12,5 +14,42 @@ emailBox.addEventListener('focus',myfunc)
 emailBox.addEventListener('focusout',(e)=>{
     if(!emailBox.value){
         inputLable.classList.remove('event-class');  
+        emailBoxContainer.classList.remove('outline-add')
     }
+})
+
+
+const hoveredElement = document.querySelector('.lang-list')
+hoveredElement.addEventListener('mouseover',(e)=>{
+    if (e.target.tagName.toLowerCase() === 'span' && e.target.classList.contains('language-option') && e.target === hoveredElement.firstElementChild){
+        e.target.classList.add('hover-effect-1')
+    }
+    else{
+        e.target.classList.add('hover-effect-2')
+    }
+  
+})
+
+
+hoveredElement.addEventListener('mouseout',(e)=>{
+    if (e.target.tagName.toLowerCase() === 'span' && e.target.classList.contains('language-option') && e.target === hoveredElement.firstElementChild){
+        e.target.classList.remove('hover-effect-1')
+    }
+    else{
+        e.target.classList.remove('hover-effect-2')
+    }
+  
+})
+
+
+const languageButton = document.querySelector('.btn-language');
+languageButton.addEventListener('click',()=>{
+    hoveredElement.classList.toggle('show')
+})
+
+document.addEventListener('click',(e)=>{
+    if(!languageButton.contains(e.target) && !hoveredElement.contains(e.target)){
+        hoveredElement.classList.remove('show');
+    }
+
 })
